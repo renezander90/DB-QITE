30ca1c8944a8304e13651e024b578fc567de4658
Rene Zander <rene.zander@fokus.fraunhofer.de>
2025-03-20 12:54:18 +0100

    Added use_statevector option

--- run_QITE.py
+++ run_QITE.py
@@ -23,11 +23,12 @@ from qrisp.qite import QITE
 from qrisp import QuantumVariable
 
 
-def run_QITE(H, U_0, exp_H, s_values, steps, method='GC'):
+def run_QITE(H, U_0, exp_H, s_values, steps, method='GC', use_statevectors=False):
 
     start = time.time()
 
     H_squared = H**2
+    H_matrix = H.to_array()
 
     theta = sp.Symbol('theta')
     optimal_s = [theta]
@@ -37,13 +38,18 @@ def run_QITE(H, U_0, exp_H, s_values, steps, method='GC'):
     runtimes = []
     statevectors = []
 
-    qv = QuantumVariable(H.find_minimal_qubit_amount())
+    N = H.find_minimal_qubit_amount()
+    qv = QuantumVariable(N)
     U_0(qv)
     qc = qv.qs.compile()
     circuits.append(qc)
 
-    E_0 = H.get_measurement(qv,precision=0.01,precompiled_qc=qc,diagonalisation_method='commuting')
-    V_0 = H_squared.get_measurement(qv,precision=0.01,precompiled_qc=qc,diagonalisation_method='commuting') - E_0**2
+    if use_statevectors:
+        E_0, _, V_0 = compute_moments(get_statevector(qc,N),H_matrix)
+    else:
+        E_0 = H.get_measurement(qv,precision=0.01,precompiled_qc=qc,diagonalisation_method='commuting')
+        V_0 = H_squared.get_measurement(qv,precision=0.01,precompiled_qc=qc,diagonalisation_method='commuting') - E_0**2
+
     optimal_energies.append(E_0)
     variances.append(V_0)
 
@@ -57,13 +63,20 @@ def run_QITE(H, U_0, exp_H, s_values, steps, method='GC'):
 
         # Find optimal evolution time 
         # Use "precompliled_qc" keyword argument to avoid repeated compilation of the QITE circuit
-        energies = [H.get_measurement(qv,precision=0.01,subs_dic={theta:s_},precompiled_qc=qc,diagonalisation_method='commuting') for s_ in s_values]
+        if use_statevectors:
+            energies = [compute_moments(get_statevector(qc,N,subs_dic={theta:s_}),H_matrix)[0] for s_ in s_values]
+        else:
+            energies = [H.get_measurement(qv,precision=0.01,subs_dic={theta:s_},precompiled_qc=qc,diagonalisation_method='commuting') for s_ in s_values]
+
         index = np.argmin(energies)
         s_min = s_values[index]
 
         optimal_s.insert(-1,s_min)
         optimal_energies.append(energies[index])
-        variances.append(H_squared.get_measurement(qv,precision=0.01,subs_dic={theta:s_min},precompiled_qc=qc,diagonalisation_method='commuting') - energies[index]**2)
+        if use_statevectors:
+            variances.append(compute_moments(get_statevector(qc,N,subs_dic={theta:s_min}),H_matrix)[2])
+        else:
+            variances.append(H_squared.get_measurement(qv,precision=0.01,subs_dic={theta:s_min},precompiled_qc=qc,diagonalisation_method='commuting') - energies[index]**2)
 
         end = time.time()
         runtimes.append(end-start)
@@ -86,19 +99,28 @@ def run_QITE(H, U_0, exp_H, s_values, steps, method='GC'):
         circuit_qubits[k]=tqc.num_qubits()
         circuit_depth[k]=tqc.depth()
 
-        if k>0:
-            # Move the qubits to the top
-            n = H.find_minimal_qubit_amount()
-            for i in range(qc.num_qubits() - n):
-                qc.qubits.insert(0, qc.qubits.pop(-1))
-
-            # Compute the unitary
-            statevectors.append(qc.statevector_array()[:2**n])
-        else:
-            statevectors.append(qc.statevector_array())
+        statevectors.append(get_statevector(qc,N))
 
     circuit_data = [circuit_ops, circuit_qubits, circuit_depth, circuit_qubits]
 
     result_dict = {'evolution_times':evolution_times,'optimal_energies':optimal_energies,'variances':variances,'circuit_data':circuit_data,'runtimes':runtimes,'statevectors':statevectors}
 
     return result_dict
+
+
+def get_statevector(qc,n,subs_dic=None):
+    if subs_dic is not None:
+        bqc = qc.bind_parameters(subs_dic) 
+    else:
+        bqc = qc
+    
+    for i in range(bqc.num_qubits() - n):
+        bqc.qubits.insert(0, bqc.qubits.pop(-1))
+
+    return bqc.statevector_array()[:2**n]
+
+def compute_moments(psi, H):
+    psi = np.array([psi]).transpose()
+    E = (psi.conj().T @ H @ psi)[0,0].real
+    S = (psi.conj().T @ H @ H @ psi)[0,0].real
+    return E, S, S - E**2
